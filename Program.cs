de06c67d388212025a111a628094e5aaaf92313c
Emre Can Sari <cansarrr2@gmail.com>
2022-04-08 19:10:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month; 
+                switch (month)
+         {
+                case 1:
+                Console.WriteLine("Ocak Ayındasınız");
+                break;
+                case 4:
+                Console.WriteLine("Nisan Ayındasınız");
+                break;
+            
+               
+             
+             default:
+             break;
+         }
+         switch (month)
+         {
+             case 12:
+             case 1:
+             case 2:
+                Console.WriteLine("Kış  Ayındasınız");
+                break;
+             case 3:
+             case 4:
+             case 5:
+                Console.WriteLine("İlk Bahar  Ayındasınız");
+                break;
+             
+             default:
+             break;
+         }
+        }
+    }
+}
